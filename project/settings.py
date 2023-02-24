3f72a3878ddd996299e44671110cff4f5129b793
Abderrahman Es <68553016+abdrahmanES1@users.noreply.github.com>
2023-02-24 21:03:24 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -139,6 +139,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICURL_DIRS = [
